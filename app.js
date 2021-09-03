216f5767af6126bb6b3ca8ed427fcebd8b8184ec
Abhinay Singh <77923855+Abhinay5180@users.noreply.github.com>
2021-09-04 04:04:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -43,9 +43,10 @@ app.use(express.json());
 // MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost/formData',(err,client)=>db=client.db('formData'));
 
 app.get('/',(req,res)=>{
-formData.find({}).toArray((err,result)=>{
-    console.log(err);
-   res.render('pages/formdatas.ejs',{formdatadetails : result}); 
+formData.find({},(err,formdatas)=>{
+ res.render('pages/formdatas.ejs',{formdatadetails : formdatas});    
+})
+   
 })
 
 // Delete Route
